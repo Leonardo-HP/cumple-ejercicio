e04b80a2a9402cc87d13182cd8376af896a224d9
Leonardo HP <ba.leonardohp@gmail.com>
2022-12-15 11:41:19 -0600

    Comentarios

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
-
+//documentacion en hooks https://reactjs.org/docs/hooks-state.html
 
 function App() {
 
 
-//declaramos las variables y su manejador de estado;
+//declaramos las variables y su manejador de estado con un hook
 
 //el primer parametro de la constante es el estado
 
@@ -25,11 +25,17 @@ const [info,setInfo] = useState("");
 //la modificacion del valor " <button onClick={() => setNombre(nombre = "Leonardo")}>""
 
 
+
+//esta funcion componente que nuestro boton submit va a realizar cuando hagamos click en submit 
 const handleSubmit =(e) =>{
   e.preventDefault()
 
+
+  //desde aqui podemos modificar el comportamiento de nuestros modificadores de estado en este caso actualizamos el set info
+  //para imprimirlo despues 
+
   //set info esta modificando el valor de info y lo esta cambiando por la informacion otorgada dentro de su modificador
-setInfo(nombre+fecha+locacion)
+    setInfo(`Hola ${nombre} tu fecha de nacimiento es ${fecha} y naciste en ${locacion}.`)
 
 }
 
@@ -41,33 +47,31 @@ setInfo(nombre+fecha+locacion)
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Unicamente imprimimos la informacion
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-    
-        >
-          Learn React
-        </a>
-
+      
+      
 
 
   <form onSubmit={handleSubmit}>
     <label> ¿Cuál es tu nombre?
-      <input type="text" value={nombre} placeholder="aqui va tu nombre" onChange={(e)=>setNombre(e.target.value)}/>
+
+    {/* aquieestamos usando  un evento on change para utilizar nuestro modificador para que nombre se convierta en el valor dentro de nuestro input 
+        al igual que se hizo con input setNombre esta tomando la informacion dentro para modificar el nombrea*/}
+
+{/* aunque definir el valor es redundante por que el modificador esta definiendo la constante, por ahora lo dejamos para manejar que visualmente el codigo*/}
+
+      <input type="text"  value={nombre} placeholder="aqui va tu nombre" onChange={(e)=>setNombre(e.target.value)}/>
     </label>
 
-
+    <br></br>
     <label> ¿Cuando naciste?
     <input type="date" value={fecha} placeholder="aqui va tu fecha" onChange={(e)=>setFecha(e.target.value)}/>
     </label>
-
+    <br></br>
 
 	 <label> ¿Dónde naciste?
-	 <select  value={locacion} onSubmit={(e)=>setLocacion(e.target.value)}> 
+	 <select  value={locacion} onChange={(e)=>setLocacion(e.target.value)}> 
    <option value="">Selecciona tu ciudad</option>
     <option value="Puebla,MX">Puebla MX</option>
     <option value="NewYork,US">Nueva York US</option>
@@ -83,16 +87,9 @@ setInfo(nombre+fecha+locacion)
 
 <p>{info}</p>
 
-
-
-
       </header>
 
   
-
-
-
-
     </div>
   );
 }
